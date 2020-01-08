41367779fbd857e395ba6782ecf8ba86249593c1
wangbiao <wangbiao1012@gmail.com>
2020-01-08 15:14:22 +0800

    注册中心开发完成

--- alibaba-nacos-provider-server/src/main/java/com/after00/AlibabaNacosProviderServerApplication.java
+++ alibaba-nacos-provider-server/src/main/java/com/after00/AlibabaNacosProviderServerApplication.java
@@ -1,17 +1,22 @@
 package com.after00;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.io.IOException;
+import java.net.URI;
 
 /**
  * @RestController 表示控制层
@@ -25,25 +30,12 @@ class AlibabaNacosProviderServerApplication {
         SpringApplication.run(AlibabaNacosProviderServerApplication.class, args);
     }
 
-    @Slf4j
-    @RestController
-    static class TestController {
-
-        @GetMapping("/echo/{name}")
-        public String echo(@PathVariable String name) {
-            return "hello " + name;
-        }
-        @Autowired
-        LoadBalancerClient loadBalancerClient;
-
-        @GetMapping("/test")
-        public String test() {
-            // 通过spring cloud common中的负载均衡接口选取服务提供节点实现接口调用
-            ServiceInstance serviceInstance = loadBalancerClient.choose("alibaba-nacos-discovery-server");
-            String url = serviceInstance.getUri() + "/hello?name=" + "didi";
-            RestTemplate restTemplate = new RestTemplate();
-            String result = restTemplate.getForObject(url, String.class);
-            return "Invoke : " + url + ", return : " + result;
-        }
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
+
+
+
 }
